3c396706af8e6c4f98db8a89b51e7a6eeb8041e9
SJMosley <samueljmosley@gmail.com>
2025-04-15 12:45:34 -0500

    text_to_text_nodes working, general split image/link logic for single spot maintenance

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,6 +1,18 @@
 import re
 from textnode import TextNode, TextType
 
+def text_to_text_nodes(text):
+    # print("\n🚨🚨🚨")
+    # print(f"nodes \n{nodes}")
+    nodes = [TextNode(text, TextType.TEXT, None)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+#This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     if delimiter not in "_**`":
         raise ValueError(f"invalid delimiter: {delimiter}")
@@ -27,14 +39,111 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     #NON RECURSIVE
     return new_nodes
 
-def simple_markdown_extract(pattern, text):
-    return re.findall(pattern, text)
+def split_nodes_logic(extraction_func, old_nodes, extracted_node_type=None):
+    #Same logic for image and link functions, pulling it out
+    #Extraction func must return Tuple of (text, url)
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        current_text = old_node.text
+        extracted_items = extraction_func(current_text)
+        if extracted_items == []:
+            new_nodes.append(old_node)
+            continue
+        for text, url in extracted_items:
+            if extracted_node_type == TextType.IMAGE:
+                item_delim = f"![{text}]({url})"
+            elif extracted_node_type == TextType.LINK:
+                item_delim = f"[{text}]({url})"
+            else:
+                raise Exception("no delimiter for type provided")
+
+            left, item, right = current_text.partition(item_delim)
+            if left != "":
+                split_nodes.append(TextNode(left, TextType.TEXT))
+            if item:
+                split_nodes.append(TextNode(text, extracted_node_type, url))
+            if right or right == "":
+                current_text = right
+        if current_text != "":
+            split_nodes.append(TextNode(current_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    return split_nodes_logic(extract_markdown_images, old_nodes, TextType.IMAGE)
+    # new_nodes = []
+    # # "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+
+    # for old_node in old_nodes:
+    #     #create a split_nodes to manage the nodes generated here
+    #     split_nodes = []
+
+    #     #extract images
+    #     current_text = old_node.text
+    #     images = extract_markdown_images(current_text)
+    #     if images == []:
+    #         new_nodes.append(old_node)
+    #         continue
+    #     for text, url in images:
+    #         img_delim = f"![{text}]({url})"
+    #         left, img, right = current_text.partition(img_delim)
+    #         if left != "":
+    #             split_nodes.append(TextNode(left, TextType.TEXT))
+    #         if img:
+    #             split_nodes.append(TextNode(text, TextType.IMAGE, url))
+    #         if right or right == "":
+    #             current_text = right
+    #     if current_text != "":
+    #         split_nodes.append(TextNode(current_text, TextType.TEXT))
+    #     new_nodes.extend(split_nodes)
+    # return new_nodes
+
+def split_nodes_link(old_nodes):
+    return split_nodes_logic(extract_markdown_links, old_nodes, TextType.LINK)
+    # new_nodes = []
+
+    # for old_node in old_nodes:
+    #     split_nodes=[]
+
+    #     current_text = old_node.text
+    #     links = extract_markdown_links(current_text)
+    #     # print(f"🔗links {links}")
+    #     if links == []:
+    #         # print(f"old_node {old_node}")
+    #         new_nodes.append(old_node)
+    #         continue
+    #     for text, url in links:
+    #         link_delim = f"[{text}]({url})"
+    #         left, lnk, right = current_text.partition(link_delim)
+    #         # print(f"left {left}")
+    #         # print(f"img {lnk}")
+    #         # print(f"right {right}")
+
+    #         if left != "":
+    #             split_nodes.append(TextNode(left, TextType.TEXT))
+    #         if lnk:
+    #             split_nodes.append(TextNode(text, TextType.LINK, url))
+    #         if right or right == "":
+    #             current_text = right
+    #     if current_text != "":
+    #         split_nodes.append(TextNode(current_text, TextType.TEXT))
+    #     new_nodes.extend(split_nodes)
+
+    # return new_nodes
 
 def extract_markdown_images(text):
-    return simple_markdown_extract(r"!\[(.*?)\]\((.*?)\)", text)
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
 
 def extract_markdown_links(text):
-    return simple_markdown_extract(r"\[(.*?)\]\((.*?)\)", text)
+    pattern = r"\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
 
 #Printed Tests from the lesson
 # text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
@@ -44,3 +153,25 @@ def extract_markdown_links(text):
 # text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
 # print(extract_markdown_links(text))
 # # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+#
+# print("\n🚨🚨🚨")
+# node = TextNode(
+#         "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+#         TextType.TEXT,
+#     )
+# print(split_nodes_image([node]))
+# print("\n🚨🚨🚨")
+# linknode = TextNode(
+#     "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+#     TextType.TEXT,
+# )
+# print(split_nodes_link([linknode]))
+# print("\n🚨🚨🚨")
+# [
+#     TextNode("This is text with a link ", TextType.TEXT),
+#     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+#     TextNode(" and ", TextType.TEXT),
+#     TextNode(
+#         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+#     ),
+# ]
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -1,8 +1,26 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, split_nodes_link, split_nodes_image, text_to_text_nodes
 
 class TestSplitTextNode(unittest.TestCase):
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_text_nodes(text)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        )
     def test_split_text_no_inline(self):
         node = TextNode("This is a text block with no splitter", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -78,9 +96,41 @@ class TestSplitTextNode(unittest.TestCase):
                 TextNode(" word", TextType.TEXT),
             ]
         )
-    # def test_split_link(self):
-    # def test_split_image(self):
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with an [link](https://i.imgur.com/zjjcJKZ.png) and another [second link](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
 
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second link", TextType.LINK, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
 
 # Normal text/Text
 # **Bold text**
