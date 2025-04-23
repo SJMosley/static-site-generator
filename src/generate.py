f386fbcfb0f2e43986eaa593b70276047134d3f0
SJMosley <samueljmosley@gmail.com>
2025-04-22 23:27:35 -0500

    recursive generation for content pages

--- src/generate.py
+++ src/generate.py
@@ -15,14 +15,34 @@ def generate_page(from_path, template_path, destination_path):
 
     template = template.replace("{{ Title }}", title)
     result_html = template.replace("{{ Content }}", content)
-    print(template)
+    # print(template)
 
     #destination_path and making dirs
-    print(f"dest_path {destination_path}")
+    # print(f"dest_path {destination_path}")
     dest_dir_name = os.path.dirname(destination_path)
-    print(f"dest_dir_name {dest_dir_name}")
+    # print(f"dest_dir_name {dest_dir_name}")
     if not os.path.exists(dest_dir_name):
         os.makedirs(dest_dir_name)
 
     with open(destination_path, 'w') as destination_file:
         destination_file.write(result_html)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    dir_items = os.listdir(dir_path_content)
+    for item in dir_items:
+        item_path = os.path.join(dir_path_content, item)
+        dest_path = os.path.join(dest_dir_path, item)
+        if os.path.isdir(item_path):
+            generate_pages_recursive(item_path, template_path, dest_path)
+            continue
+        # if not os.path.isfile(item_path):
+        #     generate_pages_recursive(item_path, template_path, dest_dir_path)
+        #     continue
+        if '.md' in item:
+            html_path = dest_path.replace(".md", ".html")
+            # print("\n🚨🚨🚨")
+            # print(f"dest_path {dest_path}")
+            # print(f"html_path {html_path}")
+            # print("\n🚨🚨🚨")
+            generate_page(item_path, template_path, html_path)
+            continue
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 from textnode import TextNode, TextType
 from build_site import publish_content
-from generate import generate_page
+from generate import generate_pages_recursive, generate_page
 def main():
     copy_from_dir = './static' #from_dir
     dest_dir = './public' #to_dir
-    content_file = './content/index.md'
+    content_dir = './content'
     template_file =  './template.html'
-    dest_file = './public/index.html'
+    dest_dir = './public'
+    # content_file = './content/index.md'
+    # dest_file = './public/index.html'
 
     publish_content(copy_from_dir, dest_dir)
-    generate_page(content_file, template_file,dest_file)
+    generate_pages_recursive(content_dir, template_file, dest_dir)
+    # generate_page(content_file, template_file, dest_file)
 
 main()
